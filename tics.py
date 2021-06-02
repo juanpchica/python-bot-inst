4bb1cc0176c623fc49ec49beb119fb351976ab78
juanpchica <juanpchica@hotmail.com>
2021-06-02 14:21:30 -0500

    Last condition added

--- tics.py
+++ tics.py
@@ -3,8 +3,12 @@ def cuandoVacunarse(edad, ocup, personasCasa):
   elif (edad > 65 and edad <= 80) or ocup == "Contratista" or (personasCasa >= 3 and personasCasa <= 7): print("Se agendara para Julio")
   elif (edad > 40 and edad <= 65) or ocup == "Pensionado" or (personasCasa >= 8 and personasCasa <= 11): print("Se agendara para Septiembre")
   elif (edad < 40 and edad >= 1) or ocup == "Desempleado" or personasCasa == 1 and personasCasa == 2 : print("Se agendara para diciembre")
+  else: print("Los datos digitados no son validos en nuestro sistema...")
 
-cuandoVacunarse(58, "Contratista", 2)
-cuandoVacunarse(81, "Empleado", 5)
-cuandoVacunarse(60, "Pensionado", 8)
-cuandoVacunarse(25, "Desempleado", 1)
+
+edad = int(input("¿Edad del paciente? "))
+ocup = input("¿Ocupación del Paciente? ")
+personasCasa = int(input("¿Número de personas con las que vive? "))
+
+cuandoVacunarse(edad,ocup, personasCasa)
+
