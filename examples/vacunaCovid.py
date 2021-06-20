b6eebd6e86efe5310d07e3dbd0d732b3c2f3aeea
juanpchica <juanpchica@hotmail.com>
2021-06-19 19:23:07 -0500

    First Validation

--- examples/vacunaCovid.py
+++ examples/vacunaCovid.py
@@ -3,3 +3,13 @@ edad = input(':')
 ocupacion = input(':')
 zona = input(':')
 cantPersonas = input(':')
+
+countEnseguida = 0
+countJulio = 0
+countSeptiembre = 0
+countDiciembre = 0
+
+if edad > 80: countEnseguida =+ countEnseguida
+elif edad > 65 & edad <=80: countJulio =+ countJulio
+elif edad >40 & edad >= 65: countSeptiembre =+ countSeptiembre
+else:  countDiciembre =+ countDiciembre
