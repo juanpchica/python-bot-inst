2244dbe8ee055e986d7790f68ff999647956001b
juanpchica <juanpchica@hotmail.com>
2021-06-25 08:12:24 -0500

    Printing month from array

--- examples/pobreza.py
+++ examples/pobreza.py
@@ -3,10 +3,17 @@ nombre = input('Ingrese Nombre:')
 
 opcion = input('Desea continuar procesando información? Escriba Si/No')
 
-cont = 1;
+cont = 1
+meses = []
+
 while opcion.lower() == 'si' and cont <=5:
     print(f'Por favor ingrese la información para el mes #{cont}')
-    mes = input('Nombre mes:')
+    mesNombre = input('Nombre mes:')
     ingreso = input('Ingreso del mes:')
     personas = input('Cantidad de personas que dependen del ingreso:')
 
+    #guardo datos en una lista
+    meses.append(mesNombre,int(ingreso),int(personas))
+
+for mes in meses:
+    print(mes.mesNombre)
