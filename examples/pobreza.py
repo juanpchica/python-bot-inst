2fb94c27879e63c901160e843d58fc537c3d3321
juanpchica <juanpchica@hotmail.com>
2021-06-25 09:02:34 -0500

    Validate salary below 800000

--- examples/pobreza.py
+++ examples/pobreza.py
@@ -1,21 +1,39 @@
-cc = input('Ingrese cc:')
-nombre = input('Ingrese Nombre:')
-
 opcion = input('Desea continuar procesando información? Escriba Si/No: ')
 
 cont = 1
-meses = []
+encuestado = []
+encuestados = []
 
 while opcion.lower() == 'si' and cont <=5:
     print(f'Por favor ingrese la información para el mes #{cont}')
-    mesNombre = input('Nombre mes:')
+    mes = input('Nombre mes:')
     ingreso = input('Ingreso del mes:')
     personas = input('Cantidad de personas que dependen del ingreso:')
 
+    #Valido margen de pobreza
+    if ingreso <= 800000:
+        if personas == 1 : margenPobreza = 'Clase media'
+        elif personas >= 2 and personas <=5 : margenPobreza = 'Pobre'
+        elif personas >= 6 : margenPobreza = 'Muy pobre'
+
+
+
     #guardo datos en una lista
-    meses.append((mesNombre,int(ingreso),int(personas)))
+    encuestado.append((mes,int(ingreso),int(personas)))
 
     cont +=1
+    if cont == 5:
+        #Calculo datos finales para un encuestado
+        for mes in encuestado:
+            print(mes[0])
 
-for mes in meses:
-    print(mes[0])
+
+
+        #Agrego encuestado a lista
+        encuestados.append(encuestado)
+        opcion = input('Desea continuar procesando información? Escriba Si/No: ')
+        if opcion.lower() == 'si' : cont = 1
+
+
+#Calculo datos finales de todos los encuestados
+
