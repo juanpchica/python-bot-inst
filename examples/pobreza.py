74391d9771c81f669a47418444f530ed34423349
juanpchica <juanpchica@hotmail.com>
2021-06-25 16:09:49 -0500

    Finalizando ejercicio, mostrando valores finales

--- examples/pobreza.py
+++ examples/pobreza.py
@@ -83,7 +83,7 @@ while opcion.lower() == 'si' and cont <=5:
         promedio = totalIngreso / len(encuestado)
 
         #Muestro resultados finales para este encuestado
-        print(f'Para promedio de sus ingresos es de {promedio}')
+        print(f'\n\nPara promedio de sus ingresos es de {promedio}')
         print(f'El mes con mayor ingreso fue {encuestado[posMaxIngreso][0]} con un total de {encuestado[posMaxIngreso][1]}')
         print(f'El mes con menor ingreso fue {encuestado[posMinIngreso][0]} con un total de {encuestado[posMinIngreso][1]}')
 
@@ -95,9 +95,9 @@ while opcion.lower() == 'si' and cont <=5:
 
         #Agrego encuestado a lista con todos los valores calculados
         encuestados.append((encuestado,promedio,encuestado[posMaxIngreso][0],encuestado[posMinIngreso][0],listaMargenPobreza[max_index]))
-        opcion = input('Desea continuar procesando información? Escriba Si/No: ')
+        opcion = input('\nDesea continuar procesando información? Escriba Si/No: ')
         if opcion.lower() == 'si':
-            cont = 1
+            cont = 0
             encuestado = []
             contMuyPobre = 0
             contPobre = 0
@@ -108,3 +108,62 @@ while opcion.lower() == 'si' and cont <=5:
     cont += 1
 
 #Calculo datos finales de todos los encuestados
+totalEncuestadosIngresos = 0
+contMuyPobre = 0
+contPobre = 0
+contClaseMedia = 0
+contRico = 0
+contMuyRico = 0
+ListMesesEncuestadosMayor = []
+ListMesesEncuestadosMenor = []
+
+#Recorro cada encuestado y calculo datos finales
+for enc in encuestados :
+    totalEncuestadosIngresos += enc[1]
+
+    # Calculo el margen de pobreza mas frecuente
+    if enc[4] == 'Muy pobre': contMuyPobre += 1
+    elif enc[4] == 'Pobre': contPobre += 1
+    elif enc[4] == 'Clase Media': contClaseMedia += 1
+    elif enc[4] == 'Rico': contRico += 1
+    elif enc[4] == 'Muy Rico': contMuyRico += 1
+
+    #Agrego el mes mas o menos representativo
+    ListMesesEncuestadosMayor.append(enc[2])
+    ListMesesEncuestadosMenor.append(enc[3])
+
+
+#Realizo promedio de ingresos para todos los encuestados
+promedioTotal = totalEncuestadosIngresos / len(encuestados)
+
+#Calculo el margen de pobreza mas comun en todos los encuestados
+listaMargenPobrezaComunEncuestados = [contMuyPobre, contPobre, contClaseMedia, contRico, contMuyRico]
+max_value = max(listaMargenPobrezaComunEncuestados)
+max_index = listaMargenPobrezaComunEncuestados.index(max_value)
+
+#Calculo el mes mas representativo
+counter = 0
+mesMasRepresentativo = ListMesesEncuestadosMayor[0]
+
+for val in ListMesesEncuestadosMayor:
+    curr_frecuencia = ListMesesEncuestadosMayor.count(val)
+    if(curr_frecuencia > counter):
+        counter = curr_frecuencia
+        mesMasRepresentativo = val
+
+
+#Calculo el mes menos representativo
+counter = 0
+mesMenosRepresentativo = ListMesesEncuestadosMenor[0]
+
+for val in ListMesesEncuestadosMenor:
+    curr_frecuencia = ListMesesEncuestadosMenor.count(val)
+    if(curr_frecuencia > counter):
+        counter = curr_frecuencia
+        mesMenosRepresentativo = val
+
+#Imprimo valores finales
+print(f'\nEl ingreso promedio de los encuestados es de {promedioTotal} ')
+print(f'Margen de pobreza promedio es {listaMargenPobreza[max_index]} ')
+print(f'El mes más representativo en ingresos fue {mesMasRepresentativo} ')
+print(f'El mes menos representativo en ingresos fue {mesMenosRepresentativo} ')
