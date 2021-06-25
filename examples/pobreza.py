9d051ccb675d0794173183e613794e2e63fae260
juanpchica <juanpchica@hotmail.com>
2021-06-25 08:08:14 -0500

    Interfaz de preguntas al usuario agregada

--- examples/pobreza.py
+++ examples/pobreza.py
@@ -1,5 +1,12 @@
 cc = input('Ingrese cc:')
 nombre = input('Ingrese Nombre:')
 
+opcion = input('Desea continuar procesando información? Escriba Si/No')
 
+cont = 1;
+while opcion.lower() == 'si' and cont <=5:
+    print(f'Por favor ingrese la información para el mes #{cont}')
+    mes = input('Nombre mes:')
+    ingreso = input('Ingreso del mes:')
+    personas = input('Cantidad de personas que dependen del ingreso:')
 
